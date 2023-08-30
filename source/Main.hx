943fc578f26520803727a21b53fe36bae54e4480
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-08-30 14:08:01 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -89,8 +89,13 @@ class Main extends Sprite
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
+		#if android
+		addChild(new FlxGame(1280,720,initialState,60,60,skipSplash,startFullscreen))
+		#else
+		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		#end
+				     
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
